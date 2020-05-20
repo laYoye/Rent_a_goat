b5b5409694e3d83c13dd27664fdbedcddc5b617a
Frederique Berthier <frederique.berthier@gmail.com>
2020-05-20 13:12:09 +0200

    changement des jpeg en jpg et création show

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,10 @@ puts "Cleaning database..."
 Herd.destroy_all
 
 puts "Creating herds..."
-chevrettes = { name: "Les Chevrettes", photo: "chevrettes.jpeg", address: "7 rue blanche, Saint-Philibert", goat_number: 3, about: "Chevrettes joyeuses et proprettes!"  }
-seguin =  { name: "Monsieur Seguin", photo: "seguin.jpeg", address: "12 allée de la tonte, Arles",goat_number: 2, about: "Chèvres disciplinées, elles ne mangeront pas vos plates-bandes!" }
-noiraude =  { name: "La noiraude", photo: "noiraude.jpeg", address: "12 chemin principal, Fley",goat_number: 1, about: "Chèvre de caractère mais efficace!" }
-ribambelle =  { name: "La Ribambelle", photo: "ribambelle.jpeg",  address: "1 rue verte, Ergersheim",goat_number: 8, about: "Un gros troupeau, parfait pour les grands terrains!" }
+chevrettes = { name: "Les Chevrettes", photo: "chevrettes.jpg", address: "7 rue blanche, Saint-Philibert", goat_number: 3, about: "Chevrettes joyeuses et proprettes!"  }
+seguin =  { name: "Monsieur Seguin", photo: "seguin.jpg", address: "12 allée de la tonte, Arles",goat_number: 2, about: "Chèvres disciplinées, elles ne mangeront pas vos plates-bandes!" }
+noiraude =  { name: "La noiraude", photo: "noiraude.jpg", address: "12 chemin principal, Fley",goat_number: 1, about: "Chèvre de caractère mais efficace!" }
+ribambelle =  { name: "La Ribambelle", photo: "ribambelle.jpg",  address: "1 rue verte, Ergersheim",goat_number: 8, about: "Un gros troupeau, parfait pour les grands terrains!" }
 
 [ chevrettes, seguin, noiraude, ribambelle ].each do |attributes|
   herd = Herd.create!(attributes)
