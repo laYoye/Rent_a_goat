4102dd83b00210dee069df871f5684fb2b714e34
Emmanuel MARIE <emmanuel.marie@qlapoti.fr>
2020-05-20 12:46:15 +0200

    root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "dashboard", to: "dashboard#bookings"
   resources :herds, only: [:index, :show, :new, :create, :destroy] do
-    resources :bookings, only: [:index, :new, :create]
+    resources :bookings, only: [:new, :create]
   end
 end
