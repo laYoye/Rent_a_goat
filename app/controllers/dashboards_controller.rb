2b48ecdd936afbeccd3428ed7577f03110a17ba5
Yoann Vuillermet <yoann.vuillermet@gmail.com>
2020-05-21 13:01:07 +0200

    herds added to dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,11 @@
 class DashboardsController < ApplicationController
   def bookings
     @bookings = current_user.bookings
+    @herds = current_user.herds
   end
-  
+
+  def herds
+    @herds = current_user.herds
+  end
+
 end
